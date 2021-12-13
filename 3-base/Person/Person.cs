522d8df49555682b7ee0c7f03f85a5f28145c83d
GreedNeSS <ruslan199126@gmail.com>
2021-12-13 20:19:54 +0300

    Use base

---
+++ 3-base/Person/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_base
+{
+    class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public Person(string firstname, string lastname)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+        
+        public new bool Equals(Object obj)
+        {
+            Person person = (Person) obj;
+            Console.WriteLine("Работает переопределённый метод Equals");
+            return (Firstname == person.Firstname) &&
+                (Lastname == person.Lastname);
+        }
+        public string ComparePersons(Person person)
+        {
+            bool equilParams = Equals(person);
+            bool fullEquil = base.Equals(person);
+            if (fullEquil)
+            {
+                return "Абсолютно одинаковые обьекты";
+            }
+            if (equilParams)
+            {
+                return "Одинаковые свойства обьектов";
+            }
+            return "Обьекты разные";
+        }
+    }
+}
---
+++ 3-base/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3_base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Marcus", "Aurelius");
+            Object person2 = new Person("Marcus", "Aurelius");
+            Person person3 = person1;
+
+            Console.WriteLine("person1.Equils(person2): " + person1.Equals(person2));
+            Console.WriteLine("person1.Equils(person3): " + person1.Equals(person3));
+
+            Console.WriteLine("person1.ComparePersons(person2)");
+            Console.WriteLine(person1.ComparePersons((Person)person2));
+            Console.WriteLine("person1.ComparePersons(person3)");
+            Console.WriteLine(person1.ComparePersons(person3));
+        }
+    }
+}
