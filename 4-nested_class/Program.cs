be37fa4b710315d6ac0722e71c7178b59aaffafc
GreedNeSS <ruslan199126@gmail.com>
2021-12-13 21:43:10 +0300

    nested class

---
+++ 4-nested_class/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _4_nested_class
+{
+    class Home
+    {
+        public class Window
+        {
+            bool windowIsOpen = false;
+
+            public void ShutWindow()
+            {
+                if (windowIsOpen)
+                {
+                    windowIsOpen = false;
+                }
+            }
+
+            public void OpenWindow()
+            {
+                if (!windowIsOpen)
+                {
+                    windowIsOpen = true;
+                }
+            }
+
+            public void ShowWindow()
+            {
+                if (windowIsOpen)
+                {
+                    Console.WriteLine("Окно открыто");
+                }
+                else
+                {
+                    Console.WriteLine("Окно закрыто");
+                }
+            }
+        }
+
+        Window window = new Window();
+
+        public Window FrontWindow
+        {
+            get { return window; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Home home = new Home();
+            Console.WriteLine(home.FrontWindow);
+            home.FrontWindow.ShowWindow();
+            home.FrontWindow.OpenWindow();
+            home.FrontWindow.ShowWindow();
+            home.FrontWindow.ShutWindow();
+            home.FrontWindow.ShowWindow();
+        }
+    }
+}
